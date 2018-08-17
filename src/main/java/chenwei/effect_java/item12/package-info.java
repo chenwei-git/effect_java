b26ffe964076b05e720a34ca4fdfb5ddd0fdd017
chenwei <978544851@qq.com>
2018-08-18 00:27:39 +0800

    2018-08-18

---
+++ src/main/java/chenwei/effect_java/item12/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * @author chenwei
+ *
+ */
+package chenwei.effect_java.item12;
+
+// 1 始终重写 toString 方法
+// 1.1 为什么要重写, 不易读
+// 1.2 怎么重写, 格式良好, 意义简明扼要.
+// 1.3 有什么好的工具, Google的开放源代码AutoValue工具在条目 10中讨论过，它为你生成一个toString方法，
+// 就像大多数IDE工具一样。 这些方法非常适合告诉你每个属性的内容，但并不是专门针对类的含义。
---
+++ src/main/java/chenwei/effect_java/item13/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author chenwei
+ *
+ */
+package chenwei.effect_java.item13;
+// 1 谨慎地重写 clone 方法
